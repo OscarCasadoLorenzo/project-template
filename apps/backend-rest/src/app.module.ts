b68bcda291c5d820b746cb633d58c28e1a95a0d0
OSSO <oscarcl.work@gmail.com>
2025-12-15 19:15:16 +0100

    TMP-19: Implement ping health-check endpoint (#20)
    * TMP-19 feat: `ping` endpoint implemented and health module created
    
    * TMP-19 fix: update ESLint parser options for correct project path

--- apps/backend-rest/src/app.module.ts
+++ apps/backend-rest/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
 import { CommonModule } from "./common/common.module";
+import { HealthModule } from "./health/health.module";
 import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
@@ -12,6 +13,7 @@ import { PrismaModule } from "./prisma/prisma.module";
     CommonModule,
     PrismaModule,
     AuthModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
